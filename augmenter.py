8ed8a74ebf4e4a8278e46e127221bc0197883cfd
root <andrew7011616@gmail.com>
2025-04-08 18:15:49 +0000

    add files

---
+++ augmenter.py
@@ -0,0 +1,97 @@
+import numpy as np
+
+AUGMENTERS_DICT = {}
+
+def register_internal_serializable(path, symbol):
+    global AUGMENTERS_DICT
+    if isinstance(path, (list, tuple)):
+        name = path[0]
+    else:
+        name = path
+    AUGMENTERS_DICT[name] = symbol
+    
+class aug_export:
+    def __init__(self, path):
+        self.path = path
+        print('init:', path)
+
+    def __call__(self, symbol):
+        print('call:', symbol)
+        register_internal_serializable(self.path, symbol)
+        return symbol
+
+@aug_export('LowPassFilter_Det')
+class LowPassFilterDeterministic():
+    def __init__(self, data_freq=4, highcut_hz=0.05):
+        """
+        Apply low pass filter to remove frequency bands >= highcut_hz
+        :param data_freq: frequency of data to apply filter to (e.g., 4Hz for EDA)
+        :param highcut_hz: lower bound on frequency bands to remove
+        """
+        self.data_freq = data_freq
+        self.highcut_hz = highcut_hz
+        self.b, self.a = scipy.signal.butter(4, [highcut_hz], btype="lowpass", output="ba", fs=data_freq)
+
+    def __call__(self, x):
+        # print(x.shape)
+        segment_filtered = scipy.signal.filtfilt(self.b, self.a, x, axis=0)
+        return segment_filtered.astype(np.float32)
+
+@aug_export('GaussianNoise_Det')
+class GaussianNoiseDeterministic:
+    def __init__(self, sigma_scale=0.1):
+        """
+        :param sigma_scale: factor to use in computing sigma parameter for noise distribution
+            sigma = mean(abs(diff between signal & mean))) * sigma_scale
+        """
+        self.sigma_scale = sigma_scale
+
+    def __call__(self, x):
+        # x = np.squeeze(x, -1)
+        mean_power_diff = np.mean(np.abs(x - np.mean(x)))
+        noise_sigma = mean_power_diff * self.sigma_scale
+        noise = np.random.normal(scale=noise_sigma, size=x.shape)
+        # print(noise_sigma)
+        return (x + noise).astype(np.float32)
+
+@aug_export('GaussianNoise_Sto')
+class GaussianNoiseStochastic:
+    def __init__(self, sigma_scale_min=0.0, sigma_scale_max=0.5):
+        """
+        :param sigma_scale_min: min factor to use in computing sigma parameter for noise distribution
+        :param sigma_scale_max: max factor to use in computing sigma parameter for noise distribution
+            sample sigma_scale uniformly in [sigma_scale_min, sigma_scale_max)
+            sigma = mean(abs(diff between signal & mean))) * sigma_scale
+        """
+        self.sigma_scale_min = sigma_scale_min
+        self.sigma_scale_max = sigma_scale_max
+
+    def __call__(self, x):
+        # sample sigma scale
+        # x = np.squeeze(x, -1)
+        sigma_scale = np.random.uniform(self.sigma_scale_min, self.sigma_scale_max)
+        # print(sigma_scale.shape)
+        mean_power_diff = np.mean(np.abs(x - np.mean(x)))
+        noise_sigma = mean_power_diff * sigma_scale
+        noise = np.random.normal(scale=noise_sigma, size=x.shape)
+        return (x + noise).astype(np.float32)
+
+@aug_export('BandstopFilter_Det')
+class BandstopFilterDeterministic:
+    def __init__(self, data_freq=4, remove_freq=0.25, Q=0.707):
+        """
+        Selects frequency band to remove.
+        See https://stackoverflow.com/questions/54320638/how-to-create-a-bandstop-filter-in-python
+        :param data_freq: frequency of data to apply filter to (e.g., 4Hz for EDA)
+        :param remove_freq: frequency band to remove
+        :param Q: "quality factor" Q = remove_freq / width of filter
+        see - https://docs.scipy.org/doc/scipy/reference/generated/scipy.signal.iirnotch.html
+        and https://en.wikipedia.org/wiki/Q_factor
+        """
+        self.data_freq = data_freq
+        self.remove_freq = remove_freq
+        self.Q = Q
+        self.b, self.a = iirnotch(self.remove_freq, self.Q, fs=self.data_freq)
+
+    def __call__(self, x):
+        return filtfilt(self.b, self.a, x, axis=0).astype(np.float32)
---
+++ utils.py
@@ -0,0 +1,37 @@
+import h5py
+import os
+import numpy as np 
+import tensorflow as tf
+from tensorflow.data import Dataset
+from sklearn.model_selection import train_test_split
+from glob import glob
+
+def get_dataset(data_path='./', batch_size=64, test_size=0.2, seed=42, unlabeled_aug=None, labeled_aug=None):
+    unlabeled_eda = []
+    labeled_eda = []
+    labels = []
+    for fn in glob(os.path.join(data_path, '*.h5')):
+        f = h5py.File(fn, 'r')
+        unlabeled_eda += f['eda_unlabel']
+        labeled_eda += f['eda']
+        labels += f['label']
+    # labels = list(range(len(labeled_eda)))
+    unlabeled_eda, labeled_eda = np.array(unlabeled_eda, np.float32), np.array(labeled_eda, np.float32)
+    labels = (np.array(labels)==2).astype(np.int32)
+    X_train, X_test, y_train, y_test = train_test_split(labeled_eda, labels, test_size=test_size, random_state=seed)
+    
+    unlabeled_train_ds = Dataset.from_tensor_slices(unlabeled_eda)
+    if (unlabeled_aug):
+        unlabeled_train_ds = unlabeled_train_ds.map(unlabeled_aug)
+    unlabeled_train_ds = unlabeled_train_ds.batch(batch_size).prefetch(buffer_size=tf.data.AUTOTUNE)
+    
+    labeled_train_ds = Dataset.from_tensor_slices((X_train, y_train))
+    if (labeled_aug):
+        labeled_train_ds = labeled_train_ds.map(labeled_aug)
+    labeled_train_ds = labeled_train_ds.batch(batch_size).prefetch(buffer_size=tf.data.AUTOTUNE)
+    
+    labeled_test_ds = Dataset.from_tensor_slices((X_test, y_test)) \
+        .batch(batch_size) \
+        .prefetch(buffer_size=tf.data.AUTOTUNE)
+
+    return unlabeled_train_ds, labeled_train_ds, labeled_test_ds
